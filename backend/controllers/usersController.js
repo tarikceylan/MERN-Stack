40e892df525e197ece2062239740a55f1fe072b8
tarikceylan <tarikceylan@outlook.com.tr>
2022-11-04 14:28:03 +0300

    Fix Error Handler On User Listing

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select('-password').lean();
-  if (!users) {
+  if (!users?.length) {
     return res.status(400).json({ message: 'No Users Found' });
   }
   res.json(users);
